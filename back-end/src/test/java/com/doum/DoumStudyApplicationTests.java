9b7911ee342bc17b267d79ffc6a1c7a0e067b47e
KimDongGi <kdkdongki1997@gmail.com>
2020-09-03 15:18:09 +0900

    create project

---
+++ back-end/src/test/java/com/doum/DoumStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoumStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
