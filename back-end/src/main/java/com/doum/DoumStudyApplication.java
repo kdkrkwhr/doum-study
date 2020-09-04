fa369178d09cb6606ab671c21e21fee4608cfcad
KimDongGi <kdkdongki1997@gmail.com>
2020-09-04 17:21:05 +0900

    [feat] B.E ==> SpringBoot & React 연동위한 Cors 설정 추가 및 Oauth2 추가

--- back-end/src/main/java/com/doum/DoumStudyApplication.java
+++ back-end/src/main/java/com/doum/DoumStudyApplication.java
@@ -1,12 +1,34 @@
 package com.doum;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class DoumStudyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DoumStudyApplication.class, args);
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
